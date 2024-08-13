e078e40e7c7ac5a50b4eb4853530f04772c0f1ea
Kim, Hyunseong <qwqwhsnote@gm.gist.ac.kr>
2024-08-13 13:24:34 +0900

    Additional methods

--- opttrot/c_src/pauli_bn_methods.h
+++ opttrot/c_src/pauli_bn_methods.h
@@ -1,5 +1,14 @@
 #ifndef __PAULI_BN_METHODS__
 #define __PAULI_BN_METHODS__
 
+#include "pauli_bn.h"
+
+#include <complex.h>
+
+void mat_to_coefs(double complex **mat);
+void coefs_to_mat(double complex **coefs_mat);
+
+PyObject * get_PauliList_FromCoefs(double complex **coefs_mat);
+
 
 #endif
